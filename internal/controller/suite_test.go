89a9ddaf3c30ad085fec8e87c45451c7fb637b1b
Eliad <eliasbirkedal@gmail.com>
2024-12-06 23:05:43 +0100

    Formatting
    Signed-off-by: Eliad <eliasbirkedal@gmail.com>

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -17,20 +17,20 @@ limitations under the License.
 package controller
 
 import (
-    "path/filepath"
-    "testing"
+	"path/filepath"
+	"testing"
 
-    . "github.com/onsi/ginkgo/v2"
-    . "github.com/onsi/gomega"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 
-    corev1 "k8s.io/api/core/v1"
-    "k8s.io/client-go/kubernetes/scheme"
-    "k8s.io/client-go/rest"
-    "sigs.k8s.io/controller-runtime/pkg/client"
-    "sigs.k8s.io/controller-runtime/pkg/envtest"
-    logf "sigs.k8s.io/controller-runtime/pkg/log"
-    "sigs.k8s.io/controller-runtime/pkg/log/zap"
-    //+kubebuilder:scaffold:imports
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	//+kubebuilder:scaffold:imports
 )
 
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
@@ -41,39 +41,39 @@ var k8sClient client.Client
 var testEnv *envtest.Environment
 
 func TestControllers(t *testing.T) {
-    RegisterFailHandler(Fail)
+	RegisterFailHandler(Fail)
 
-    RunSpecs(t, "Controller Suite")
+	RunSpecs(t, "Controller Suite")
 }
 
 var _ = BeforeSuite(func() {
-    logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
-    By("bootstrapping test environment")
-    testEnv = &envtest.Environment{
-        CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
-        ErrorIfCRDPathMissing: false,
-    }
+	By("bootstrapping test environment")
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
+		ErrorIfCRDPathMissing: false,
+	}
 
-    var err error
-    // cfg is defined in this file globally.
-    cfg, err = testEnv.Start()
-    Expect(err).NotTo(HaveOccurred())
-    Expect(cfg).NotTo(BeNil())
+	var err error
+	// cfg is defined in this file globally.
+	cfg, err = testEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(cfg).NotTo(BeNil())
 
-    err = corev1.AddToScheme(scheme.Scheme)
-    Expect(err).NotTo(HaveOccurred())
+	err = corev1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
 
-    //+kubebuilder:scaffold:scheme
+	//+kubebuilder:scaffold:scheme
 
-    k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
-    Expect(err).NotTo(HaveOccurred())
-    Expect(k8sClient).NotTo(BeNil())
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient).NotTo(BeNil())
 
 })
 
 var _ = AfterSuite(func() {
-    By("tearing down the test environment")
-    err := testEnv.Stop()
-    Expect(err).NotTo(HaveOccurred())
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).NotTo(HaveOccurred())
 })
